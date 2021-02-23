a79006ffb30f823159fe475072d63d905604ce4b
Benjamin Harris <harben31@gmail.com>
2021-02-22 16:38:25 -0800

    finished README & added comments.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # bbh_homework_wk_3
 This is a random password generator. With a prompt it will ask how many charactors you would like it. If the user cancels or puts in a value that is outside of the given length parameters the process will start over will start over.  Once the user has input an acceptable length value they will be asked via a series of confirm pop ups whether they would like to use capitol letters, lowercase letters, numbers or special symbols. The user hits OK for 'yes' and cancel for 'no'. If user selects none of these parameters the process will start over. Once one or more of the options have been selected a password the length of which is equal to the number given by the user using the chartcer sets selected by the user will appear in the box. The first one to four charactors are garuanteed charactors to ensure that at least one of each set of charactors is represented; i.e. if the user selects upper and lower case but not numbers or symbols the first two charactors of the password will be 'Aa', if all the charactor sets are selected the password will start with 'Aa1!'.
+
+
+*[deployed web page](https://harben31.github.io/bbh_homework_wk_3/)
+
+screenshots:
+
+![screenshot1](assets/images/screenshot1.png)
+![screenshot2](assets/images/screenshot2.png)
+![screenshot3](assets/images/screenshot3.png)
+![screenshot4](assets/images/screenshot4.png)
+![screenshot5](assets/images/screenshot5.png)
+![screenshot6](assets/images/screenshot6.png)
+
+
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,16 +2,15 @@
 var generateBtn = document.querySelector("#generate");
 
 
-// document refers to the html file. the querySelector is returning 1st html element with the id of generate. the car generateBtn is refering to the html button element with the generate id. 
-
-
-
 function writePassword() {
+
+    //strings of possible charactor catagories
     var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var alphaLower = "abcdefghijklmnopqrstuvwxyz";
     var numbers = "1234567890";
     var charactors = "!\"#$%&'()*+-./<:=;>?@[]^_`{}| ~";
 
+    //empty string to be added to when the user selects a character set
     var mainString = "";
 
     var promiseString = [];
@@ -19,97 +18,78 @@ function writePassword() {
     var password = [];
 
     function generatePassword(){
-        //need to ask user 5 things when this function is called.
-        //length of password 8-128;
-        //use capitol alphabet?
-        //use lower aplphabet?
-        //use numbers?
-        //use special charactors
-
+        
         var passwordLength = prompt("how long would you\nlike your password?\n8-128 charactors",);
-        console.log(passwordLength);
 
-        // for loop i <= passwordLength.Length 
-       
+        //prompts uder for password length
         if(passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)===true){
+            //if the usert inputs a value that is not aceptable this code fires
             alert("your password must \nbe at least 8 charactors\n and no more than 128.\n please try again."); 
             generatePassword();
-            //need to create main array to choose charactors from
-            //add to main string then convert whols string to array AFTER 
+            
         } else {
+            //if the user inputs an acceptable value into the length prompt this series of code fires
             var conUpper = confirm("would you like to use\nuppercase letters?");
         } 
         
+        //each of the following code blocks work the same way if the OK button is selcted then the string with its collection of charactors is added to the main string otherwise an empty string is added.
         if(conUpper){
             var mainString2 = mainString.concat(alphaUpper);
             promiseString.push(alphaUpper[0]);
-            console.log(promiseString);
-            console.log(mainString2);
             var conLower = confirm("Would you like to use\nlowercase letters");
         }else if(!conUpper){
             var conLower = confirm("Would you like to use\nlowercase letters");
             var mainString2 = mainString.concat("");
-            console.log(mainString2);
         };
 
         if(conLower){
             var mainString3 = mainString.concat(alphaLower);
             promiseString.push(alphaLower[0]);
-            console.log(promiseString);
-            console.log(mainString3);
             var conNumber = confirm("Would you like to use\nnumbers?");
         } else if(!conLower){
             var conNumber = confirm("Would you like to use\nnumbers?");
             var mainString3 = mainString.concat("");
-            console.log(mainString3);
         }
 
         if(conNumber) {
             var mainString4 = mainString.concat(numbers);
             promiseString.push(numbers[0]);
-            console.log(promiseString);
-            console.log(mainString4);
             var conCharactor = confirm("Would you like to use\nspecial charctors?");
         } else if(!conNumber){
             var conCharactor = confirm("Would you like to use\nspecial charctors?");
             var mainString4 = mainString.concat("");
-            console.log(mainString4);
         }
 
         if(conCharactor) {
             var mainString5 = mainString.concat(charactors);
             promiseString.push(charactors[0]);
-            console.log(promiseString);
-            console.log(mainString5);
         } else if(!conCharactor){
             var mainString5 = mainString.concat("");
         }
 
         var totalString = mainString.concat(mainString2, mainString3, mainString4, mainString5);
-        console.log(totalString);
 
+        //if none of the charactor sets are selected the user is alerted and the functin will start again
         if(totalString ===""){
             alert("You gotta pick something");
             generatePassword();
         }
 
+        //the string with all the selected character sets is turned into an array
         var totalArray = [...totalString];
-        console.log(totalArray);
 
+        //the password is randomly generated
         for(i=0; i < passwordLength; i++) {
-            //just greater than or greater than equal to? it's comparing regular numbers to an index probably so it should end one lower
-            console.log("password length =" + passwordLength);
            var index = Math.floor(Math.random() * totalArray.length);
             password.push(totalArray[index]);
-            // console.log(password);
         }
 
         for(i=0; i<promiseString.length; i++){
             password[i] = promiseString[i];
         }
 
+        //the password has been generated in an array this code is turning it back into a string
         password = password.join("");
-        console.log(password);
         return password
     }
 
@@ -121,5 +101,5 @@ function writePassword() {
 
 };
 
-// Add event listener to generate button
+//fires off function
 generateBtn.addEventListener("click", writePassword, console.log("clicked"));
